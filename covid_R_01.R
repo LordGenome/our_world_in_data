4abb033f72e63cfd821aefc5683a37c3cc62fd95
LordGenome <graham.r.taylor@gmail.com>
2020-06-29 11:34:09 +0100

    minor update

--- covid_R_01.R
+++ covid_R_01.R
@@ -35,6 +35,7 @@ world <- c("GBR", "FRA", "USA", "ITA", "CHN", "BRA", "DEU")
 
 mex_bra_usa_can <- c( "BRA", "CAN", "USA", "MEX")
 
+mex_usa_can <- c( "CAN", "USA", "MEX")
 
 ## filtering by country ----
 
@@ -52,10 +53,12 @@ owid_world <- filter(owid, iso_code %in% world)
 
 owid_mex_bra_usa_can <- filter(owid, iso_code %in% mex_bra_usa_can)
 
+owid_mex_usa_can <- filter(owid, iso_code %in% mex_usa_can)
+
 
 ## filtering by date ----
 
-date_v <- seq(as.Date("2020-03-01"), as.Date("2020-06-25"), by = "days") %>% as.character() #need as.charactor %in% to work
+date_v <- seq(as.Date("2020-03-01"), as.Date("2020-06-29"), by = "days") %>% as.character() #need as.charactor %in% to work
 
 ## date range filter ----
 date_range <- filter(owid_mex_bra_usa_can, date %in% date_v)
@@ -64,36 +67,36 @@ date_range <- mutate(date_range, Date = as.Date(date)) #then back to date format
 
 ##plot new deaths or cases per million in the countries selected at date range filter ----
 ggplot(date_range) +
-  stat_smooth(mapping = aes(x = Date, y = new_cases_per_million, group = iso_code, colour = iso_code), span= 0.5, show.legend = TRUE) +
-  geom_point(mapping =  aes(x = Date, y = new_cases_per_million, colour = iso_code, shape = iso_code), show.legend = TRUE) +
+  stat_smooth(mapping = aes(x = Date, y = new_deaths_per_million, group = iso_code, colour = iso_code), span= 0.7, show.legend = TRUE) +
+  geom_point(mapping =  aes(x = Date, y = new_deaths_per_million, colour = iso_code, shape = iso_code), show.legend = TRUE) +
   theme_bw() +
   scale_x_date(NULL,
                breaks = scales::breaks_width("1 week"),
                labels = scales::label_date_short()) +
   #scale_y_continuous(name = "new cases per million", breaks = seq(0, 25, by = 5)) +
-  ylim(0, 150) +
-  ylab("new cases per million") +
+  ylim(0, 10) +
+  ylab("new deaths per million") +
   labs (title = "Covid-19 new cases per million in Brazil, Canada, Mexico and USA",
         subtitle = "Source: Our World in Data  (https://github.com/owid/covid-19-data/tree/master/public/data)",
-        caption = "March 1st to June 25th 2020 https://github.com/LordGenome/our_world_in_data")
+        caption = "March 1st to June 29th 2020 https://github.com/LordGenome/our_world_in_data")
 
 
 ##plot new deaths and cases per million in the countries selected at date range filter ----
 ggplot(date_range) +
-  stat_smooth(mapping = aes(x = Date, y = new_cases_per_million, group = iso_code), colour = "blue", span= 0.5, show.legend = TRUE) +
-  geom_point(mapping =  aes(x = Date, y = new_cases_per_million, shape = iso_code,), colour = "blue", show.legend = TRUE) +
-  #stat_smooth(mapping = aes(x = Date, y = 6*new_deaths_per_million, group = iso_code), span= 0.5, colour = "yellow", show.legend = FALSE) +
-  #geom_point(mapping =  aes(x = Date, y = 6*new_deaths_per_million, shape = iso_code), colour = "yellow", show.legend = FALSE) 
+  stat_smooth(mapping = aes(x = Date, y = new_deaths_per_million, group = iso_code, colour = iso_code), span= 0.7, show.legend = TRUE) +
+  geom_point(mapping =  aes(x = Date, y = new_deaths_per_million, shape = iso_code, colour = iso_code), show.legend = TRUE) +
+  #stat_smooth(mapping = aes(x = Date, y = new_deaths_per_million, group = iso_code), span= 0.7, colour = "yellow", show.legend = FALSE) +
+  #geom_point(mapping =  aes(x = Date, y = new_deaths_per_million, shape = iso_code), colour = "yellow", show.legend = FALSE) 
   theme_bw() +
   scale_x_date(NULL,
                breaks = scales::breaks_width("1 week"),
                labels = scales::label_date_short()) +
   #scale_y_continuous(name = "new cases per million", breaks = seq(0, 25, by = 5)) +
-  ylim(0, 125) +
+  ylim(0, 10) +
   ylab("new cases per million") +
-  labs (title = "ovid-19 new cases per million in UK, France, USA, Italy, China, Brazil & Germany",
+  labs (title = "Covid-19 new deaths per million in Brazil, Canada, Mexico & USA",
         subtitle = "Source: Our World in Data  (https://github.com/owid/covid-19-data/tree/master/public/data)",
-        caption = "March 1st to June 23rd 2020 https://github.com/LordGenome/our_world_in_data")
+        caption = "March 1st to June 28th 2020 https://github.com/LordGenome/our_world_in_data")
 
 
 
